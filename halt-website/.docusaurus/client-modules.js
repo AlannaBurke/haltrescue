221cf5f76a9f12d55b3c96af00a50c935b7f5665
Alanna Burke <alanna.burke@amazee.com>
2024-08-28 21:49:46 -0400

    Including files to see if this works.

---
+++ halt-website/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/alannaburke/Projects/haltrescue/halt-website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/alannaburke/Projects/haltrescue/halt-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/alannaburke/Projects/haltrescue/halt-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/alannaburke/Projects/haltrescue/halt-website/src/css/custom.css"),
+];
