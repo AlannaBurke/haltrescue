7cc0563b2946d39434af964b4ab0f3b0743a5ee4
Alanna Burke <alanna.burke@amazee.com>
2024-08-29 22:46:37 -0400

    Forget hosting, let's try github pages.

---
+++ halt-website/build/assets/js/36994c47.09b632d5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhalt_website=self.webpackChunkhalt_website||[]).push([[858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
