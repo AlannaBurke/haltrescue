3c1b959ca4ca34b924edbfcb9ead756671a77ff2
Alanna Burke <alanna.burke@amazee.com>
2024-10-09 13:28:19 -0400

    Deploy website - based on fef8985744af1616710483738631a279c4a6ea72

---
+++ assets/js/ef8b811a.44e161d9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhalt_website=self.webpackChunkhalt_website||[]).push([[947],{6600:e=>{e.exports=JSON.parse('{"authors":[{"name":"Alanna Burke","title":"Board Member, Foster Parent, Volunteer","page":{"permalink":"/blog/authors/aburke"},"socials":{"x":"https://x.com/aburke626"},"imageURL":"/img/aburke.jpg","key":"aburke","count":3}]}')}}]);
